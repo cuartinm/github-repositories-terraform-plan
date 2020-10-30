fb5c531178dae54ac954f9346d43741e49f48657
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-10-29 23:40:09 -0500

    change repo name

---
+++ github_team_repository.tf
@@ -0,0 +1,14 @@
+resource "github_team" "some_team" {
+  name        = "SomeTeam"
+  description = "Some cool team"
+}
+
+resource "github_repository" "some_repo" {
+  name = "some-repo"
+}
+
+resource "github_team_repository" "some_team_repo" {
+  team_id    = "${github_team.some_team.id}"
+  repository = "${github_repository.some_repo.name}"
+  permission = "pull"
+}
